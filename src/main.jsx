bade62f2786e1b2c05b019684f383d03ebfe53c9
Rohit Chandra <chandrarohit91@gmail.com>
2025-09-04 22:59:48 +0530

    initial commit: add main application structure with React components

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement('h1', {}, 'Hello World!');
 
+const MyApp = () => {
+  return (<div>
+
+    <h1>This My App</h1>
+    <Heading />
+  </div>
+  );
+}
+
+const Heading = () => {
+  return (<>{heading}
+  </>);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<MyApp />);
